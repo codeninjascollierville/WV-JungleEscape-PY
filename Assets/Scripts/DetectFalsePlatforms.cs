91959c3c16e60c4edad6ef6fe8e20568a6f258e5
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-03 18:26:47 -0600

    JungleEscapePY
    2/3/23

--- Assets/Scripts/DetectFalsePlatforms.cs
+++ Assets/Scripts/DetectFalsePlatforms.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class DetectFalsePlatforms : MonoBehaviour
 {
-
+    public bool hit;
     void Update()
     {
-
+        hit = Physics.Raycast(transform.position, transform.forward, 1.5f);
+        Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.red);
     }
+    /*void HitCheck()
+    {
+        
+    }*/
 }
