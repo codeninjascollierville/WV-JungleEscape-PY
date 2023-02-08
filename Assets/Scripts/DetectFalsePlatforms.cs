a431c10677c6a9bbd18a36c9ebe304d6a54f103f
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-07 18:45:15 -0600

    Submission
    2/7/23

--- Assets/Scripts/DetectFalsePlatforms.cs
+++ Assets/Scripts/DetectFalsePlatforms.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class DetectFalsePlatforms : MonoBehaviour
 {
     public bool hit;
+    int FalseLayer = 1 << 8;
     void Update()
     {
-        hit = Physics.Raycast(transform.position, transform.forward, 1.5f);
-        Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.red);
-    }
-    /*void HitCheck()
-    {
-        
-    }*/
+        hit = Physics.Raycast(transform.position, transform.forward, 2f, FalseLayer);
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.red);
+        if(hit)
+        {
+            Debug.LogWarning("Watch out!!");
+        }else{
+            Debug.Log("All Good");
+        }
+    }   
 }
